5c39e663872d3ebca67bc2a05879668e14a760d7
Fatema Maitham <fatemamaitham1@gmail.com>
2026-03-27 21:16:21 +0300

    Add Lab2.4

---
+++ MusicStoreMvc1/MusicStoreMvc1/Models/Album.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicStoreMvc1.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public required string Title { get; set; }
+
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 50)]
+        public int TrackCount { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; } = null!;
+
+        public int ArtistId { get; set; }
+        public Artist Artist { get; set; } = null!;
+    }
+}
